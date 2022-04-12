17e942c0f06457f1ada7a8dc974a45f6844510a2
hubvue <cckcc.kim@gmail.com>
2022-04-12 14:38:50 +0800

    feat: 将错误输出到stderr

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ mod lib;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = lib::Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = lib::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
